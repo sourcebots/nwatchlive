4c3b77f571af4e0435e0114e0789f0d21721746f
Alistair Lynn <arplynn@gmail.com>
2015-02-01 17:08:32 +0000

    Use a more sensible default service set

--- services.default.js
+++ services.default.js
@@ -1,15 +1,2 @@
-var count = 0;
-
-addWatcher('pony', function(ack, err) {
-    count += 1;
-    if (count % 4 == 2) {
-        err('out for the count');
-    } else {
-        ack();
-    }
-});
-
-addWatcher('py', watchHTTP('http://localhost:8080/irc_handlers.py'));
-
-addWatcher('Google DNS', watchPing('8.8.8.8'));
+addWatcher('Internet uplink', watchPing('8.8.8.8'));
 
