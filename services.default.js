1b6772df704aec48983e0e1932951a7825b5ac47
George Peppard <me@gpeppard.com>
2024-08-01 22:52:06 +0100

    remove v6, add soton

--- services.default.js
+++ services.default.js
@@ -1,7 +1,8 @@
 var dns = require('dns');
 
 addWatcher('Internet uplink', watchPing('8.8.8.8'));
-addWatcher('Internet uplink (v6)', watchPing6('2001:4860:4860::8888'));
+addWatcher('UoS DNS (Infoblox)', watchPing('152.78.111.81'))
+// addWatcher('Internet uplink (v6)', watchPing6('2001:4860:4860::8888'));
 addWatcher('DNS', function(ack, err) {
     dns.resolve('google.com', function(e, addresses) {
         if (e) {
